4cd3d9b3676fd3b8e506afcd6fb6ed2c80f789a6
selvaraj24 <38650968+selvaraj24@users.noreply.github.com>
2018-07-03 10:59:10 +0000

    Update Num.c

--- Codekata/Num.c
+++ Codekata/Num.c
@@ -2,12 +2,12 @@
 int main()
 {
  int n,i,sum=0;
- printf("Enter a positive integer:");
+ 
  scanf("%d",&n);
  for(i=1;i<=n;++i)
  {
   sum=sum+i;
  }
- printf("Sum=%d",sum);
+ printf(sum);
  return 0;
 }
